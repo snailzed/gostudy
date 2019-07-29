b311eba45178fad124b3537e7d92c6e9cc82267c
SnailZED <584119837@qq.com>
2019-07-29 19:51:34 +0800

    datetime

---
+++ assets/code/14-time.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	//now
+	now := time.Now()
+	fmt.Println(now)
+	format(now)
+	//当前时间戳
+	fmt.Println(now.Unix())
+
+	//时间戳转 time类型
+	var timeSeon int64 = 1564400072
+	timeObj := time.Unix(timeSeon, 0)
+	format(timeObj)
+
+	//一些常量
+	fmt.Printf("nano second:%d\n", time.Nanosecond)
+	fmt.Printf("micro second:%d\n", time.Microsecond)
+	fmt.Printf("milli second:%d\n", time.Millisecond)
+	fmt.Printf("second:%d\n", time.Second)
+
+	//格式化输出 2006-01-02 15:04:05
+	fmt.Println(now.Format("2006-01-02 15:04:05"))
+
+	//ticker定时器
+	c := time.Tick(time.Second)
+	for i := range c {
+		fmt.Println(i)
+	}
+
+}
+
+func format(now time.Time) {
+	//当前时间
+	year := now.Year()
+	month := now.Month()
+	day := now.Day()
+	hour := now.Hour()
+	min := now.Minute()
+	second := now.Second()
+	fmt.Printf("%4d-%02d-%02d %02d:%02d:%02d\n", year, month, day, hour, min, second)
+}
