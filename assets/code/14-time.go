395e895733b129028c733dd19bd3d3eb0d1d7028
SnailZED <584119837@qq.com>
2019-07-29 19:58:11 +0800

    datetime

--- assets/code/14-time.go
+++ assets/code/14-time.go
@@ -18,13 +18,13 @@ func main() {
 	timeObj := time.Unix(timeSeon, 0)
 	format(timeObj)
 
-	//一些常量
+	//一些常量 最小单位为纳秒，如果不写明单位，则默认为纳秒
 	fmt.Printf("nano second:%d\n", time.Nanosecond)
 	fmt.Printf("micro second:%d\n", time.Microsecond)
 	fmt.Printf("milli second:%d\n", time.Millisecond)
 	fmt.Printf("second:%d\n", time.Second)
 
-	//格式化输出 2006-01-02 15:04:05
+	//格式化输出 2006-01-02 15:04:05 go诞生时间
 	fmt.Println(now.Format("2006-01-02 15:04:05"))
 
 	//ticker定时器
