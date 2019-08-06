e1be8afe3d04fee29ae6978553a2446046aee9ea
SnailZED <584119837@qq.com>
2019-08-06 20:09:13 +0800

    httpserver

--- code/network/3-http-server.go
+++ code/network/3-http-server.go
@@ -8,8 +8,17 @@ import (
 func main() {
 	//启动一个http server
 	addr := "127.0.0.1:8080"
+	http.HandleFunc("/", handle)
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
 		fmt.Println("create http server error:", err)
+	}
+}
+
+//处理函数
+func handle(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte(r.URL.String()))
+	if err != nil {
+		fmt.Println(err)
 	}
 }
