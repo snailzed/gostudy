46e7a5abc78050787fdda9476c9dd43584f20459
SnailZED <584119837@qq.com>
2019-08-07 00:19:54 +0800

    http server

--- code/network/3-http-server.go
+++ code/network/3-http-server.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
 func main() {
+	startServer()
+	//startHttpServer()
+}
+
+//
+func startServer() {
 	//启动一个http server
 	addr := "127.0.0.1:8080"
 	http.HandleFunc("/", handle)
@@ -15,8 +22,17 @@ func main() {
 	}
 }
 
+//v2
+func startHttpServer() {
+	err := http.ListenAndServe(":8080", http.HandlerFunc(handle))
+	if err != nil {
+		fmt.Println("create http server error:", err)
+	}
+}
+
 //处理函数
 func handle(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "HELLo")
 	_, err := w.Write([]byte(r.URL.String()))
 	if err != nil {
 		fmt.Println(err)
